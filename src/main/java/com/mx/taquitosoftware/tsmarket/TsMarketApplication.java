238ac0dec84d0eaf5b35914bc40dd82278dbe2ac
dannielgloria <danniel.gloria@gmail.com>
2021-05-08 17:58:22 -0500

    first version

---
+++ src/main/java/com/mx/taquitosoftware/tsmarket/TsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.mx.taquitosoftware.tsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/taquitosoftware/tsmarket/TsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.taquitosoftware.tsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
