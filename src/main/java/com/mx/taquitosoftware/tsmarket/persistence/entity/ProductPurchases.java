98b192cb481998515e6919e3eba4db1ccbbc8f8f
dannielgloria <danniel.gloria@gmail.com>
2021-05-11 09:37:49 -0500

    Entity with composite PK

---
+++ src/main/java/com/mx/taquitosoftware/tsmarket/persistence/entity/ProductPurchases.java
@@ -0,0 +1,52 @@
+package com.mx.taquitosoftware.tsmarket.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "compras_productos")
+public class ProductPurchases {
+
+    @EmbeddedId
+    private ProductPurchasesPK id;
+
+    private Integer cantidad;
+
+    private BigDecimal total;
+
+    private Boolean estado;
+
+    public ProductPurchasesPK getId() {
+        return id;
+    }
+
+    public void setId(ProductPurchasesPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
