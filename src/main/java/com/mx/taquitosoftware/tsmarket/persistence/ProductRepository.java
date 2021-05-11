a6f788669084b56f9527cde95ca02f3d86603f28
dannielgloria <danniel.gloria@gmail.com>
2021-05-11 11:42:25 -0500

    Query Methods

--- src/main/java/com/mx/taquitosoftware/tsmarket/persistence/ProductRepository.java
+++ src/main/java/com/mx/taquitosoftware/tsmarket/persistence/ProductRepository.java
@@ -4,11 +4,18 @@ import com.mx.taquitosoftware.tsmarket.persistence.crud.ProductCrudRepository;
 import com.mx.taquitosoftware.tsmarket.persistence.entity.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
-    public List<Product> getAll(){
-        return (List<Product>) productCrudRepository.findAll();
+    public List<Product> getAll(){ return (List<Product>) productCrudRepository.findAll(); }
+
+    public List<Product> getByCategoria(int idCategoria){
+        return productCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Product>> getScarce(int cantidad){
+        return productCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/mx/taquitosoftware/tsmarket/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/mx/taquitosoftware/tsmarket/persistence/crud/ProductCrudRepository.java
@@ -1,7 +1,20 @@
 package com.mx.taquitosoftware.tsmarket.persistence.crud;
 
 import com.mx.taquitosoftware.tsmarket.persistence.entity.Product;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+
+    // Native way
+    //@Query(value = "SELECT * FROMA productos WHERE id_categoria = ?", nativeQuery = true)
+    //List<Product> getByCategory(int idCategoria);
+
+    //Query Methods
+    List<Product> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Product>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
