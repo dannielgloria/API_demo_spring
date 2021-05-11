6f5a9b0d2698a4a095eb12a36d7e3150bf926b1a
dannielgloria <danniel.gloria@gmail.com>
2021-05-11 11:49:48 -0500

    Implementing the @Repository notation

--- src/main/java/com/mx/taquitosoftware/tsmarket/persistence/ProductRepository.java
+++ src/main/java/com/mx/taquitosoftware/tsmarket/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.mx.taquitosoftware.tsmarket.persistence;
 
 import com.mx.taquitosoftware.tsmarket.persistence.crud.ProductCrudRepository;
 import com.mx.taquitosoftware.tsmarket.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -17,5 +19,17 @@ public class ProductRepository {
 
     public Optional<List<Product>> getScarce(int cantidad){
         return productCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Product> getProduct(int idProducto){
+        return productCrudRepository.findById(idProducto);
+    }
+
+    public Product save(Product product){
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int idProducto){
+        productCrudRepository.deleteById(idProducto);
     }
 }
