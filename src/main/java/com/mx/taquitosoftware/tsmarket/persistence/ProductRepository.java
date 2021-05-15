782f218bdfbe40a82d5addaca58d361806026dfc
dannielgloria <danniel.gloria@gmail.com>
2021-05-14 22:41:39 -0500

    Domain-oriented repository

--- src/main/java/com/mx/taquitosoftware/tsmarket/persistence/ProductRepository.java
+++ src/main/java/com/mx/taquitosoftware/tsmarket/persistence/ProductRepository.java
@@ -2,34 +2,52 @@ package com.mx.taquitosoftware.tsmarket.persistence;
 
 import com.mx.taquitosoftware.tsmarket.persistence.crud.ProductCrudRepository;
 import com.mx.taquitosoftware.tsmarket.persistence.entity.Product;
+import com.mx.taquitosoftware.tsmarket.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements com.mx.taquitosoftware.tsmarket.domain.repository.ProductRepository {
     private ProductCrudRepository productCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Product> getAll(){ return (List<Product>) productCrudRepository.findAll(); }
-
-    public List<Product> getByCategoria(int idCategoria){
-        return productCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public List<com.mx.taquitosoftware.tsmarket.domain.Product> getAll(){
+        List<Product> products = (List<Product>) productCrudRepository.findAll();
+        return mapper.toProducts(products);
     }
 
-    public Optional<List<Product>> getScarce(int cantidad){
-        return productCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    @Override
+    public Optional<List<com.mx.taquitosoftware.tsmarket.domain.Product>> getByCategory(int categoryId) {
+        List<Product> products = productCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(products));
     }
 
-    public Optional<Product> getProduct(int idProducto){
-        return productCrudRepository.findById(idProducto);
+    @Override
+    public Optional<List<com.mx.taquitosoftware.tsmarket.domain.Product>> getScarseProducts(int quantity) {
+        Optional<List<Product>> products = productCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return products.map(products1 -> mapper.toProducts(products1));
+    }
+
+    @Override
+    public Optional<com.mx.taquitosoftware.tsmarket.domain.Product> getProduct(int productId) {
+
+        return productCrudRepository.findById(productId).map(product -> mapper.toProduct(product));
+    }
+
+    @Override
+    public com.mx.taquitosoftware.tsmarket.domain.Product save(com.mx.taquitosoftware.tsmarket.domain.Product product) {
+        Product product1 = mapper.toProducto(product);
+        return mapper.toProduct(productCrudRepository.save(product1));
     }
 
     public Product save(Product product){
         return productCrudRepository.save(product);
     }
 
-    public void delete(int idProducto){
-        productCrudRepository.deleteById(idProducto);
+    public void delete(int productId){
+        productCrudRepository.deleteById(productId);
     }
 }
